9f7b3efada4917d867ca7564888e3e301232f240
Andi Leman <leman@lcr-reader.cn>
2022-06-24 18:37:08 +0800

    Website edits 2022-06-20-1658

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 //import { NavDropdown } from 'react-bootstrap';
-import {Navbar, Nav, NavDropdown, Container, Col, Row, Button} from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Container, Col, Row, Button, Form, FormControl} from 'react-bootstrap'
 import {NavLink, Link} from 'react-router-dom';
 import Logo from '../components/Logo'
+
+const expand = 1
 
 const HeaderNav: React.FC = () => {
   //justifyContent:'center', alignItems:'center', alignContent:'center',
@@ -10,17 +12,36 @@ const HeaderNav: React.FC = () => {
     return (
       <Navbar bg='white' expand='sm' fixed='top' sticky="top" className='me-auto justify-content-center'>
         <Container>
-          <Navbar.Brand as={NavLink} to='/'>
-            <img alt='' src='ai-rox-logo.png' width='150' height='auto' className='d-inline-block align-center'/>
+          <Navbar.Brand as={NavLink} to='/' className='d-inline-block align-center'>
+            <img alt='' src='ai-rox-logo.png' width='150' height='auto' />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav'/>
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='me-auto' style={{textAlign:'center'}}>
+            <Nav className='me-auto justify-content-end'>
               <Nav.Link as={NavLink} to='engineering'>Engineering</Nav.Link>
               <Nav.Link as={NavLink} to='manufacturing'>Manufacturing</Nav.Link>
-              <Nav.Link as={NavLink} to='supply/materials'>Materials</Nav.Link>
-              <Nav.Link as={NavLink} to='supply/equipment'>Equipment</Nav.Link>
+              <Nav.Link as={NavLink} to='supply'>Supply</Nav.Link>
+               <NavDropdown title="Resources" id="navbarScrollingDropdown" className='active'>
+                <NavDropdown.Item as={NavLink} to='supply/materials'>Materials</NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to='supply/equipment'>Equipment</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  More links
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* <Nav.Link as={NavLink} to='supply/materials'>Materials</Nav.Link> */}
+              {/* <Nav.Link as={NavLink} to='supply/equipment'>Equipment</Nav.Link> */}
             </Nav>
+            &nbsp;&nbsp;
+            <Form className="d-flex">
+              <FormControl
+                type="email"
+                placeholder="mail@me"
+                className="me-2"
+                aria-label="Email"
+              />
+              <Button variant="outline-success">Contact</Button>
+            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -28,6 +49,20 @@ const HeaderNav: React.FC = () => {
 }
 
 /*
+           <NavDropdown
+                title="Dropdown"
+                id={`offcanvasNavbarDropdown-expand-${expand}`}
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+
               <NavDropdown title='supply'> 
                 <Nav.Link as={NavLink} to='supply/materials'>Materials</Nav.Link>
                 <Nav.Link as={NavLink} to='supply/equipment'>Equipment</Nav.Link>
@@ -43,7 +78,7 @@ const HeaderNav: React.FC = () => {
 export default HeaderNav
 
 /*
-
+<Navbar.Text> Signed in as: <a href="#login">Mark Otto</a> </Navbar.Text>
  <nav className='headerNav'>
         <Container style={{margin:'auto', whiteSpace: 'nowrap',  }}>
           <Row style={{textAlign:'center'}}>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ const HomePage: React.FC = () => {
                 <br/>
                 <h2 style={{fontFamily: 'forArticle'}}>Software Development, Electronic Design and Manufacturing Solutions</h2>
                 <br/>
-                <p>Years of quality electronic design and manufacturing of Printed Circuit Boards (PCBs) specializing in diverse high-tech industries.</p>
+                <p>Years of quality electronic design and manufacturing specializing in diverse high-tech industries, supporting your product life-cycle.</p>
                 <br/>
               </Col>
             </Row>
@@ -37,24 +37,25 @@ const HomePage: React.FC = () => {
         </section>
         <section>
           <Container style={{paddingTop: '30px', margin: 'auto'}}>
-            <Row>
-              <Col xs="12" style={{textAlign: 'center'}}>
+            <Row style={{height: '400px', textAlign: 'center', alignContent:'center', alignItems: 'center'}}>
+              <Col xs="12" >
                 <h1 className='ai-rox-green'>AI-ROX Polytronics</h1>
-                <p>* An Independent Design House (IDH) founded in 2018.</p>
+                <h2>* An Independent Design House (IDH) founded in 2018 an based in South China, Shenzhen.</h2>
                 <br/>
-                <h3 className='ai-rox-green'>We Are</h3>
-                <p>Electronics experts focused on scalability, efficiency, and speed.<br/>Providing cutting-edge electronics and a flexible workflow.</p>
+                <h2 className='ai-rox-green'>We Are</h2>
+                <h3>Electronics experts focused on scalability, efficiency, and speed.</h3>
+                <h3>Providing cutting-edge electronics and a flexible workflow.</h3>
               </Col>
             </Row>
-            <Row>
+            <Row style={{minHeight: '400px'}}>
               <Col>
                 <CardGroup style={{justifyContent:'center'}}>
                   <Link to='engineering' style={{padding: '10px', textDecoration: 'none'}}>
                     <Card style={{border: '1px black', boxShadow:  '2px 2px 20px 2px rgba(0,0,0,.48)'}}>
                       <Card.Img height='272' variant='top' src='engineering.webp'/>
                       <Card.Body>
-                        <Card.Title>Engineering</Card.Title>
-                        <Card.Text>from the concept to the product &gt;</Card.Text>
+                        <Card.Title className='ai-rox-home-card-title'>ENGINEERING</Card.Title>
+                        <Card.Text className='ai-rox-home-card-text'>FROM THE CONCEPT TO THE PRODUCT &gt;</Card.Text>
                       </Card.Body>
                     </Card>
                   </Link>
@@ -62,8 +63,8 @@ const HomePage: React.FC = () => {
                     <Card style={{border: '1px black', boxShadow: '2px 2px 20px 2px rgba(0,0,0,.48)'}}>
                       <Card.Img height='272' variant='top' src='Manufacturing-SMT-PCBA.webp'/>
                       <Card.Body>
-                        <Card.Title>Manufacturing</Card.Title>
-                        <Card.Text>on your behalf under supervision<br/> of our professional surveyor &gt;</Card.Text>
+                        <Card.Title className='ai-rox-home-card-title'>MANUFACTURING</Card.Title>
+                        <Card.Text className='ai-rox-home-card-text'>PROTOTYPE TO MASS-PRODUCE &gt;</Card.Text>
                       </Card.Body>
                     </Card>
                   </Link>
@@ -71,8 +72,8 @@ const HomePage: React.FC = () => {
                   <Card style={{border: '1px black', boxShadow: '2px 2px 20px 2px rgba(0,0,0,.48)'}}>
                     <Card.Img height='272' variant='top' src='marterials supply.webp'/>
                     <Card.Body>
-                      <Card.Title>Supply</Card.Title>
-                      <Card.Text>of the quality raw materials, <br/>electronic components,<br/> new and refurbished equipment &gt;</Card.Text>
+                      <Card.Title className='ai-rox-home-card-title'>SUPPLY</Card.Title>
+                      <Card.Text className='ai-rox-home-card-text'>CHEMICALS, COMPONENTS, TOOLS &gt;</Card.Text>
                     </Card.Body>
                   </Card>
                   </Link>
@@ -80,9 +81,51 @@ const HomePage: React.FC = () => {
               </Col>
             </Row>
           </Container>
-          
         </section>
-        <section>AND ANOTHER SECTION</section>
+        <section>
+          <Container>
+            <Row  style={{minHeight: '400px', textAlign: 'center', alignContent:'center', alignItems: 'center'}}>
+              <Col>
+                <h1 className='ai-rox-green'>Industries Served</h1>
+                <h2>We had honor of working with an extremely diverse spectrum of industries.</h2>
+                <br/>
+                {/* <h2 className='ai-rox-green'>We Are</h2>
+                <h3>Electronics experts focused on scalability, efficiency, and speed.</h3>
+                <h3>Providing cutting-edge electronics and a flexible workflow.</h3> */}
+              </Col>
+            </Row>
+            <Row>
+              <Col>Industrial Equipment</Col>
+              <Col>Test Equipment</Col>
+              <Col></Col>
+              <Col></Col>
+            </Row>
+            <Row>
+              <Col></Col>
+              <Col></Col>
+              <Col></Col>
+              <Col></Col>
+            </Row>
+
+          </Container>
+        </section>
+        <section>
+          <Container>
+            <Row style={{minHeight: '400px', textAlign: 'left', alignContent:'center', alignItems: 'top', margin: '10px' }}>
+              <Col xs='4'>
+                <h3>Quality Commitment</h3>
+                <p>Our mission is to continually improve our Quality Management System to provide products and services that are perfect, on time, and within budget, while improving customer satisfaction in a great working environment.</p>
+              </Col>
+              <Col xs='4' style={{color:"white", backgroundColor:'rgba(136,170,0)'}}>
+                <h3>High Tech Solutions</h3>
+                <p>Onshore precision, designing, and building products for cutting edge industries such as IoT, Aerospace, Unmanned Systems, Medical Devices, Military and Defense, Scientific and Research Equipment, and Energy Systems.</p>              </Col>
+              <Col xs='4'>
+                <h3>Communication</h3>
+                <p>Our outstanding midwest customer service and customer-focused methodology keep our client&apos;s goals front and center throughout the design and manufacturing processes.</p>
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </>
   )
 }
