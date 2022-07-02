4813b3ac811f1bbd538fe0d42acc91a711d4dd44
Andi Leman <leman@lcr-reader.cn>
2022-07-03 03:38:25 +0800

    Website edits 2022-06-20-1658

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
 import HomePage from './pages/HomePage'
 import EngineeringPage from './pages/EngineeringPage'
